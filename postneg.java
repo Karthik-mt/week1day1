b84f0da5e1b6dfdd0c9216675732b28a292b614b
Karthik-mt <91802987+Karthik-mt@users.noreply.github.com>
2021-10-09 12:59:54 +0530

    Create postneg.java

---
+++ postneg.java
@@ -0,0 +1,16 @@
+# week1day1
+assignment
+package week1.day1;
+
+public class postneg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num=-5;
+if(num>=0)
+	System.out.println("The number is postive");
+else
+	System.out.println("The number is negative");
+	}
+
+}
